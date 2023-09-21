4d95647dfd6fbe368dfc7953283a3f2dd46a84e9
poojasingh <hi4palspoja@gmail.com>
2023-09-21 12:59:22 +0545

    Done with the create, update, destroy and revise in form

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -6,9 +6,32 @@ class IdeasController < ApplicationController
         #@idea=Idea.create(description: params[:idea][:description], author:params[:idea][:author])
         #Same as above
         @idea = Idea.create(idea_params)
+        if @idea.valid?
+            #implement later
+        else
+            #implement later
+        end
         #You can redirect users to a certain page in your application with redirect_to.
         redirect_to root_path
     end
+    def edit
+        @idea = Idea.find(params[:id])
+    end
+
+    def update
+        @idea = Idea.find(params[:id])
+        if @idea.update(idea_params)
+          redirect_to root_path
+        else
+          redirect_to edit_idea_path(params[:id])
+        end
+    end
+    def destroy
+        @idea = Idea.find(params[:id])
+        @idea.destroy
+        redirect_to root_path
+    end
+
 
     private
     def idea_params
